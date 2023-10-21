1d7c824536063393eccd59608956766ffe992840
Yamer <niceroMK@gmail.com>
2023-10-21 16:46:12 +0700

    litle

--- DollHouse/Assets/Cod/Player/StairClimb.cs
+++ DollHouse/Assets/Cod/Player/StairClimb.cs
@@ -15,7 +15,7 @@ public class StairClimb : MonoBehaviour
      {
          rigidBody = GetComponent<Rigidbody>();
 
-         stepRayUpper.transform.position = new Vector3(stepRayUpper.transform.position.x, stepHeight, stepRayUpper.transform.position.z);
+      //   stepRayUpper.transform.position = new Vector3(stepRayUpper.transform.position.x, stepHeight, stepRayUpper.transform.position.z);
      }
 
      private void FixedUpdate()
@@ -27,34 +27,34 @@ public class StairClimb : MonoBehaviour
      void stepClimb()
      {
          RaycastHit hitLower;
-         if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward), out hitLower, 0.1f))
+         if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward), out hitLower, 0.3f))
          {
-             RaycastHit hitUpper;
-             if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(Vector3.forward), out hitUpper, 0.2f))
+            Debug.DrawRay(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward) * 0.3f, Color.red);
+            RaycastHit hitUpper;
+             if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(Vector3.forward), out hitUpper, 0.5f))
              {
-                 rigidBody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+                Debug.DrawRay(stepRayUpper.transform.position, transform.TransformDirection(Vector3.forward) * 0.5f, Color.green);
+                rigidBody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
              }
          }
 
          RaycastHit hitLower45;
-         if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitLower45, 0.1f))
-         {
-
-             RaycastHit hitUpper45;
-             if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitUpper45, 0.2f))
-             {
+         if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitLower45, 0.3f))
+         {                  
+            RaycastHit hitUpper45;
+             if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitUpper45, 0.5f))
+             {  
                  rigidBody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
              }
          }
 
          RaycastHit hitLowerMinus45;
-         if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitLowerMinus45, 0.1f))
+         if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitLowerMinus45, 0.3f))
          {
-
-             RaycastHit hitUpperMinus45;
-             if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitUpperMinus45, 0.2f))
+            RaycastHit hitUpperMinus45;
+             if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitUpperMinus45, 0.5f))
              {
-                 rigidBody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+                rigidBody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
              }
          }
      }
