aa5325b6156049690be5a303343651c9a15e7206
Yamer <niceroMK@gmail.com>
2024-01-18 10:53:29 +0700

    HeadBob

---
+++ Thesis ตัวร้าย/Dollmaker/Assets/New Code/HeadBobController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadBobController : MonoBehaviour
+{
+    [SerializeField] private bool _enable = true;
+    [SerializeField, Range(0,0.1f)] private float _amplitude = 0.015f;
+    [SerializeField, Range(0, 30)] private float _frequency = 10.0f;
+    [SerializeField] private Transform _camera = null;
+    [SerializeField] private Transform _cameraHolder = null;
+
+    private Vector3 _startPos;
+    private CharacterController _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        _startPos = _camera.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!_enable) return;
+        CheckMotion();
+        ResetPosition();
+        _camera.LookAt(FocusTarget());
+    }
+    private void PlayMotion(Vector3 motion)
+    {
+        _camera.localPosition += motion;
+    }
+
+    private void CheckMotion()
+    {
+        float speed = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).magnitude;
+        if (speed <= 0) return;
+        if(!_controller.isGrounded) return;
+        PlayMotion(FootStepMotion());
+    }
+
+    private Vector3 FootStepMotion()
+    {
+        Vector3 pos = Vector3.zero;
+        pos.y += Mathf.Sin(Time.time * _frequency) * _amplitude;
+        pos.x += Mathf.Cos(Time.time * _frequency / 2) * _amplitude * 2;
+        return pos;
+    }
+    
+    private Vector3 FocusTarget()
+    {
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y + _cameraHolder.localPosition.y, transform.position.z);
+        pos += _cameraHolder.forward * 15.0f;
+        return pos;
+    }
+    private void ResetPosition()
+    {
+        if(_camera.localPosition == _startPos) return;
+        _camera.localPosition = Vector3.Lerp(_camera.localPosition, _startPos, 10 * Time.deltaTime);
+    }
+
+}
