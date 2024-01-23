6242755ac2dd6ec795e71310f96a7cb024b04ab1
Yamer <niceroMK@gmail.com>
2024-01-23 18:47:11 +0700

    Prototype minigame

--- Thesis ตัวร้าย/Dollmaker/Assets/New Code/MimiGame/BallInBox.cs
+++ Thesis ตัวร้าย/Dollmaker/Assets/New Code/MimiGame/BallInBox.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class BallInBox : MonoBehaviour
 {
-    public float passBoxCount;
+    public float passBoxCount, RandomPassBox;
     public MiniGame CountpassBox;
+    public GameObject[] HitBox;
+    private bool pass, Example = true, Reset;
 
     private void Update()
     {
         if(passBoxCount >= 9) passBoxCount = 0;
+        if (Reset)
+        {
+            passBoxCount = 0;
+            Reset = false;
+        }
+        if (!pass)
+        {
+            RandomPassBox = Random.Range(1, 4);
+            pass = true;
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -18,6 +30,44 @@ public class BallInBox : MonoBehaviour
         {
             passBoxCount++; 
             CountpassBox.PassBoxcheck();
+            pass = false;
+            if (Example)
+            {
+                if (RandomPassBox == 2 && passBoxCount != 9)
+                {
+                    CountpassBox.AddBoxNumber(passBoxCount);
+                }
+            }
+            
         }
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "BoxSkillCheck")
+        {
+            if(!Example) 
+            {
+                if (Input.GetKey(KeyCode.Mouse0))
+                {
+                    CountpassBox.CurrectBoxCheck();
+                }
+            }
+        }
+    }
+
+    public void EndExample()
+    {
+        Example = false;
+    }
+
+    public void StartExample()
+    {
+        Example = true; 
+    }
+
+    public void ResetPassBox()
+    {
+        Reset = true;
     }
 }
--- Thesis ตัวร้าย/Dollmaker/Assets/New Code/MimiGame/MiniGame.cs
+++ Thesis ตัวร้าย/Dollmaker/Assets/New Code/MimiGame/MiniGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public enum MiniGameState { Start, ClearSkillCheck, FailSkillCheck, FinishSkillCheck};
 
@@ -10,14 +11,17 @@ public class MiniGame : MonoBehaviour
     private static MiniGame Instance;
     [Header("Animation skill check")]
     [SerializeField] public Animator BallAnimate;
+    public BallInBox CheckBallInBox;
 
     [Header("Score")]
     public float Total;
     public float TotalFinish;
     public float PassboxExample, PassBox; // For check to make random.
+    Queue<float> SkillCheckPass = new Queue<float>();
+    [SerializeField] private TMP_Text BoxNum; // For check number of box
 
-    private bool Example = true;
-    private bool PlayAnimate = true;
+    private bool Example = true, PlayAnimate = true, CurrectBox;
+    private float ClickBoxCount;
 
     private void Start()
     {
@@ -38,18 +42,39 @@ public class MiniGame : MonoBehaviour
             {
                 _CurrentState = MiniGameState.ClearSkillCheck;
                 PassboxExample = 0;
+                ClickBoxCount = 0;
                 Example = false;
             }
         }
         
         if(_CurrentState == MiniGameState.ClearSkillCheck)
         {
-            
+            if(PassBox >= 9)
+            {
+                PassBox = 0;
+                _CurrentState = MiniGameState.FailSkillCheck;
+            }
+            if(SkillCheckPass.Peek() < PassBox )
+            {
+                _CurrentState = MiniGameState.FailSkillCheck;
+            }
+            if (CurrectBox)
+            {
+                SkillCheckPass.Dequeue();
+                TellNumbox();
+                StartCoroutine(Delayclick());
+                CurrectBox = false;
+            }
         }
 
         if(_CurrentState == MiniGameState.FailSkillCheck)
         {
-
+            PassBox = 0;
+            Example = true; 
+            PlayAnimate = true;
+            SkillCheckPass.Clear();
+            CheckBallInBox.ResetPassBox();
+            _CurrentState = MiniGameState.Start;
         }
 
         if(_CurrentState == MiniGameState.FinishSkillCheck)
@@ -64,12 +89,53 @@ public class MiniGame : MonoBehaviour
         if (Example)
         {
             PassboxExample++;
+            CheckBallInBox.StartExample();
         }
         else
         {
             PassBox++;
+            CheckBallInBox.EndExample();
         }
     }
+
+    public void CurrectBoxCheck()
+    {
+        if (ClickBoxCount == 0)
+        {
+            if (PassBox == SkillCheckPass.Peek())
+            {
+                CurrectBox = true;
+                ClickBoxCount++;
+            }
+            else if(PassBox !=  SkillCheckPass.Peek())  
+            {
+                _CurrentState = MiniGameState.FailSkillCheck;
+                ClickBoxCount++;
+            }
+        }
+    }
+
+    public void AddBoxNumber(float ThisBox)
+    {
+        SkillCheckPass.Enqueue(ThisBox);
+        TellNumbox();
+    }
+
+    void TellNumbox()
+    {
+        BoxNum.text = string.Empty;
+        foreach (float NumberBox in SkillCheckPass)
+        {
+            BoxNum.text += NumberBox + ", ";
+        }
+    }
+
+    IEnumerator Delayclick()
+    {
+        yield return new WaitForSeconds(0.5f);
+        ClickBoxCount = 0;
+    }
+
     #endregion
 
 }
