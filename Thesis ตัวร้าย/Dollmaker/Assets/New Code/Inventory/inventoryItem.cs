a890fab2513dff5b070e504986cd6ffc053de422
Yamer <niceroMK@gmail.com>
2024-02-21 18:12:09 +0700

    New inventory unfinish

---
+++ Thesis ตัวร้าย/Dollmaker/Assets/New Code/Inventory/inventoryItem.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class inventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    
+
+    [Header("Image")]
+    public Image image;
+
+    [HideInInspector] public Item item;
+    [HideInInspector] public int Count = 1;
+    [HideInInspector] public Transform parentAfterDrag;
+
+
+    public void InitialiseItem(Item newItem)
+    {
+        item = newItem;
+        image.sprite = newItem.image;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = false;
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        image.raycastTarget=true;
+        transform.SetParent(parentAfterDrag);
+    }
+
+}
