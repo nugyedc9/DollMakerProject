e0a193fc4d7c50369f2188ab03d596e5a02804fa
Yamer <niceroMK@gmail.com>
2024-02-28 17:02:32 +0700

    Story ADd

---
+++ Thesis ตัวร้าย/Dollmaker/Assets/New Code/Story/DocumentID.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DocumentID : MonoBehaviour
+{
+    public int DocID;
+}
---
+++ Thesis ตัวร้าย/Dollmaker/Assets/New Code/Story/StoryActive.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StoryActive : MonoBehaviour
+{
+
+    public UnityEvent EventActive;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            EventActive.Invoke();
+            Destroy(gameObject);
+        }
+    }
+
+    public void DeleteAnother()
+    {
+        Destroy(gameObject);
+    }
+
+}
