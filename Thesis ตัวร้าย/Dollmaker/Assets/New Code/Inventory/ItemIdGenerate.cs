da3a5a053e5fca126c294cc90286b1462dc05030
Yamer <niceroMK@gmail.com>
2024-03-29 07:22:50 +0700

    can pick item if drop

--- Thesis ตัวร้าย/Dollmaker/Assets/New Code/Inventory/ItemIdGenerate.cs
+++ Thesis ตัวร้าย/Dollmaker/Assets/New Code/Inventory/ItemIdGenerate.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class ItemIdGenerate : MonoBehaviour
 {
     [SerializeField] public string id;
+    public bool GenerateAwake;
     [ContextMenu("Generate grid for id")]
     private void GenerateGuid()
     {
         id = System.Guid.NewGuid().ToString();
+    }
+
+    public void Awake()
+    {
+        if (GenerateAwake)
+        {
+            GenerateGuid();
+        }
     }
 
 }
